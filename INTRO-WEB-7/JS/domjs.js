d05f8fed5567374fe2023c79aad50a4841c0c3aa
Daniela-Ru <dsruiz11@gmail.com>
2023-02-10 12:18:35 -0600

    00Nuevo repositorio, agregando la carpeta INTRO-WEB-7

---
+++ INTRO-WEB-7/JS/domjs.js
@@ -0,0 +1,5 @@
+function evento (){
+    document.write("Hola esta es una introduccion al js con fuunciones y dom");
+    alert("ya fuimos al pipirin");
+
+}
---
+++ INTRO-WEB-7/JS/js01-practica-variables/tiposdedatosyvariables.js
@@ -0,0 +1,14 @@
+var nombre= "Daniela";
+var edad= 30;
+var tengomascotas=true;
+var tengotrabajo=false;
+var tengosueldo;//undefinied
+
+let nombre= "Daniela";
+let edad=30;
+let tengomascotas=true;
+let tengotrabajo=false;
+let tengosueldo;//undefinied
+
+
+
