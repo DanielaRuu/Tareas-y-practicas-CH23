ee12ecc85a3dbb685591b3a0205f245e336892bd
Daniela-Ru <dsruiz11@gmail.com>
2023-02-13 01:04:58 -0600

    03 TAREAS LISTAS INTRO-WEB-7

---
+++ INTRO-WEB/INTRO-WEB-7/Ejercicios-de-tarea/Ejercicio2-intro-bootstrap.js
@@ -0,0 +1,9 @@
+function evento (){
+    alert("¿Sabias que...? El primer robot humanoide se llamó Elektro y se construyó en 1939. Era una máquina de dos metros que contaba con una grabación de 700 palabras para que el artefacto simulara que establecía una conversación.");
+}
+
+function evento1 (){
+    alert("¿Sabias que...? El primer nombre que conocemos de la historia fue Kushim y perteneció a un sumerio de la segunda mitad del cuarto milenio antes de Cristo. Este nombre aparece en una tablilla de arcilla, en escritura pictográfica cuneiforme y es el primero del que tenemos constancia.");
+}
+
+
